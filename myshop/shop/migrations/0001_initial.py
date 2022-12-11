571bca5d1d2394a82b567e43581f4340182591b4
prostochel23 <116502447+prostochel23@users.noreply.github.com>
2022-12-11 18:20:20 +0300

    Add files via upload

---
+++ myshop/shop/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
+                ('image', models.ImageField(blank=True, upload_to='products/%Y/%m/%d')),
+                ('description', models.TextField(blank=True)),
+                ('price', models.DecimalField(max_digits=10, decimal_places=2)),
+                ('stock', models.PositiveIntegerField()),
+                ('available', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(related_name='products', to='shop.Category')),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+    ]
